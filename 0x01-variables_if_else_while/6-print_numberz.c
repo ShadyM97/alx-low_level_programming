f40af9316967a0ddd19743370bfcc4ea2c5ad3d1
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-18 02:04:38 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Positive or Negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+		putchar;
+	putchar('\n');
+	return (0);
+}
