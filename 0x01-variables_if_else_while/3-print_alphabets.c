f086e9b3f79da3b54a818e48bae9eca20a132a9c
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-18 01:50:36 +0000

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Positive or Negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 97 ; alphabet < 123 ; alphabet++)
+		putchar(alphabet);
+	for (alphabet = 65 ; alphabt < 91 ; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
