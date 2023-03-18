171b451595dd776d34f29dbddec2997573b3bab6
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-18 01:35:28 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Positive or Negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+	for(alphabet=97;alphabet<123;alphabet++)
+		putchar(alphabet);
+
+	return (0);
+}
