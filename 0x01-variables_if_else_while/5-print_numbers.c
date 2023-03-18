127da125ec9f373e3162bd0f8090f77bef825aff
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-18 02:01:59 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Positive or Negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 0 ; num < 10 ; num++)
+		printf("%d", num);
+	putchar('\n');
+	return (0);
+}
