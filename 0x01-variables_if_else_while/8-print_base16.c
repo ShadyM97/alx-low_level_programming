c5474a67f49d9d99264d17eae9353892a0c74723
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-18 02:13:19 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Positive or Negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char base_16;
+
+	for (base_16 = '0' ; base_16 <= '9' ; base_16++)
+		putchar(base_16);
+	for (base_16 = 'a' ; base_16 <= 'f' ; base_16++)
+		putchar(base_16);
+	putchar('\n');
+	return (0);
+}
