ad0162b0d707cb96905af140ea1c0916b2ef77c2
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-18 02:08:40 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+ * main - Positive or Negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z' ; alphabet <= 'a' ; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
