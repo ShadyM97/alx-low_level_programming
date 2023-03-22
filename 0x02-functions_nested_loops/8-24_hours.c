7a9e95104eb149235ca79cbd09a9268f9511f12f
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-22 13:49:41 +0000

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * jack_baur - Print mins of day
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int m;
+	for (i = 0 ; i < 3 ; i++)
+	{
+		_putchar('0' + i);
+		for (j = 0 ; j < 10 ; j++)
+		{
+			_putchar('0' + j);
+			_putchar(':');
+			for (k = 0 ; k < 6 ; k++)
+			{
+				_putchar('0' + k);
+				for (m = 0 ; m < 10 ; m++)
+				{
+					_putchar('0' + m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
