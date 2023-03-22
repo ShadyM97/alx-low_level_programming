322135530ab3bab37c108d7687b01c09d4049445
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-22 13:08:42 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_sign - Checks sign
+ * @n: character to be checked
+ *
+ * Return: 1 if character is +ve. 0 if zero. -1 if -ve.
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else
+		return (-1);
+}
