90259d0bd16e28930dc0a52480cd9f678fc41eb4
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-23 02:00:01 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,9 +12,37 @@ void times_table(void)
 	{
 		for (j = 0 ; j < 10 ; j++)
 		{
-			_putchar('0' + (i * j));
-			_putchar(',');
-			_putchar(' ');
+			if ((i * j) >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + ((i * j) / 10));
+				_putchar('0' + ((i * j) % 10));
+			}
+			else
+			{
+				if (i == 0)
+				{
+					_putchar('0' + (i * j));
+					if (j < 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+				else if (j == 0)
+				{
+					 _putchar('0' + (i * j));
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (i * j));
+				}
+			}
 		}
 		_putchar('\n');
 	}
