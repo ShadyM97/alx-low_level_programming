870330f789aa9c52b35bdae2dce1ba27c11e30c8
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-24 02:35:19 +0000

    more fun

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * more_numbers- prints the numbers.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+	int flag;
+
+	flag = 0;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j < 15 ; j ++)
+		{
+			if (j > 9)
+			{
+				_putchar('1');
+				flag=10;
+			}
+			_putchar('0' + (j - flag));
+		}
+		_putchar('\n');
+	}
+}
