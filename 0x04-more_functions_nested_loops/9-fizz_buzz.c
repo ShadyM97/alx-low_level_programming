7f7639743ceae362ecda6982774a479d270a85bf
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-25 00:50:19 +0000

    more fun

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (num = 1; num < 101; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("Fizz Buzz ");
+			printf("FizzBuzz ");
 		}
 		else if (num % 3 == 0)
 		{
