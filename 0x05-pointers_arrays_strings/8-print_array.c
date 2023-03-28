31bad50b577f11837d22c81db54251282dafbaa5
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-28 01:42:17 +0000

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,7 +10,7 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
@@ -27,6 +27,6 @@ void print_array(int *a, int n)
 	}
 	else
 	{
-		printf("\n\n");
+		printf("\n");
 	}
 }
