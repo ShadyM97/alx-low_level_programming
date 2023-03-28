8dead77a0eb666c5b1d9f35bac6a573ca303a3ec
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-28 01:57:57 +0000

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,19 +6,18 @@
  *@dest: pointer to destination
  *@src: pointer to source
  *
- * Return: the pointer to dest 
+ * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	char *ptr = dest;
-	
+
 	while (*src != '\0')
 	{
 		*dest = *src;
 		dest++;
 		src++;
 	}
-
 	*dest = '\0';
 	return (ptr);
 
