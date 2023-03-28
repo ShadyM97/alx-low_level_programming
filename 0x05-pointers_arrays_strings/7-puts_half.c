5bb06837ce8129b5ee75cbe982b7a1c1473695fb
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-28 01:13:22 +0000

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * puts_half - function that prints second half pf string
+ *@s: pointer to string
+ *
+ * Return: void
+ */
+void puts_half(char *s)
+{
+	int count;
+	int i;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	if (count % 2 == 0)
+	{
+		for (i = count / 2; i < count; i++)
+		{
+			_putchar(s[i]);
+		}
+	}
+	else
+	{
+		for (i = (count - 1) /2 ; i < count; i++)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
