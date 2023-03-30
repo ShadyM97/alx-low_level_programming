25a22bafc2d223b0819c609c33cf09caf92b1281
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-03-30 19:08:51 +0000

    more

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
+	char* ptr = dest;
+
 	if (dest == NULL)
 	{
 		return (NULL);
 	}
-
 	while ((*src != '\0') && n--)
 	{
 		*dest = *src;
@@ -23,5 +24,5 @@ char *_strncpy(char *dest, char *src, int n)
 		src++;
 	}
 	*dest = '\0';
-	return (dest);
+	return (ptr);
 }
