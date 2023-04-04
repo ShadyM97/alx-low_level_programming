e320c7372d3ac343afba8e949f3ff974608e19f7
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-04 02:17:27 +0000

    even more

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * _strpbrk - function that locates a substring
+  * @haystack: source array pointer
+  * @needle: compare string
+  * Return: pointer to the byte s on match
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *ptr1 = haystack;
+		char *ptr2 = needle;
+
+		while (*ptr1 == *ptr2 && *ptr2 != '\0')
+		{
+			ptr1++;
+			ptr2++;
+		}
+		if (*ptr2 == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return ('\0');
+}
