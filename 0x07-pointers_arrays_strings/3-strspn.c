cfc496463c7f05e7ffb496bff1522606151b79d2
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-04 01:31:27 +0000

    even more

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strchr - function gets the length of a prefix substring.
+  * @s: source array pointer
+  * @accept: compare string
+  * Return: number of bytes in the initial segment
+  */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int initial_length = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				initial_length++;
+				break;
+			}
+			else if (accept[i+1] == '\0')
+				return (initial_length);
+		}
+		s++;
+	}
+	return (initial_length);
+}
