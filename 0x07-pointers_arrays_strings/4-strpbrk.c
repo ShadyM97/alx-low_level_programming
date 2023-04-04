1c8c833879213a38781adf9b563c0c46bf5e80d4
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-04 01:50:31 +0000

    even more

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * __strpbrk - function that searches a string for any of a set of bytes
+  * @s: source array pointer
+  * @accept: compare string
+  * Return: pointer to the byte s thhat matches one of the bytes in accept or NULL
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (0);
+}
