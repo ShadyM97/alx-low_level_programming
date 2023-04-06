69b8cade80a86fb3d01abfbd58a728bc9c7d1a52
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-06 17:54:26 +0000

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,20 +8,9 @@
 
 void _print_rev_recursion(char *s)
 {
-	int newLineFlag = 0;
-
-	if (*s == '\0' && *(s+1) != '\0')
-	{
-		newLineFlag = 1;
-	}
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
-	}
-	if (newLineFlag == 1)
-	{
-		_putchar('\n');
-		newLineFlag = 0;
 	}
 }
