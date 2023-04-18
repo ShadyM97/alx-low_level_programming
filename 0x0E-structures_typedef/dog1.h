e3df20a6d16863f2c0040c8c0cf2801fc450694d
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-18 04:30:45 +0000

    struct

--- 0x0E-structures_typedef/dog1.h
+++ 0x0E-structures_typedef/dog1.h
@@ -1,12 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
 /**
   * struct dog - dog registeration
   * @name: name of the dog
   * @age: age of the dog
   * @owner: name of the dog's owner
-  *
-  * Description: dog structure
   */
 struct dog
 {
@@ -14,4 +15,6 @@ struct dog
 	float age;
 	char* owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif /* !DOG_H */
