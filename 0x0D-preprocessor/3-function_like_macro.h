132e523aa4d2674c24fdfeaf4575dfb91222cc4b
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-18 02:54:30 +0000

    macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#define ABS(x) ((x > -1) ? (x) : (x * -1))
-
-#endif /* FUNCTION_LIKE_MACRO_H */
+#define ABS(x) ((x) > (-1) ? (x) : (x * -1))
+#endif /* !FUNCTION_LIKE_MACRO_H */
