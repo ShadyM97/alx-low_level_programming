d3031b5738e3e47b75a2c0f1e5adf793e76676d6
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-18 03:41:09 +0000

    structs

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+  * struct dog - dog registeration
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: name of the dog's owner
+  */
 struct dog
 {
 	char *name;
