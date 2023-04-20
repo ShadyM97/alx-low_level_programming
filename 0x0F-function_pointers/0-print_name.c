eef64b1c271f5d14ce57819153ead3821d251368
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-20 14:52:10 +0000

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include"function_pointers.h"
+/**
+ * print_name - function that prints a name.
+ * @name: minimum value
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	f(name);
+}
