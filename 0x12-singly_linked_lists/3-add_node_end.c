9dfe04874c93998b4a36002b9e33e96c0d403604
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-04-29 00:57:00 +0000

    free

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,7 +19,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head = new;
+		(*head) = new;
 		return (new);
 	}
 	while (temp->next)
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  * free_list - function that frees a list_t list.
+  * @head: pointer to head of list
+  * Return: void
+  */
+void free_list(const list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
