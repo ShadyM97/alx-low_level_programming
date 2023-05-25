f6c527e158f9bd5353be4b5376075e4f2a2a0648
ShadyM97 <shadymabdelkhalek@hotmail.com>
2023-05-25 08:06:22 +0000

    redemption

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,8 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int length(char *s);
+int compare(char *s, int beg, int end);
+int is_palindrome(char *s);
 
 #endif /* MAIN_H */
